7e8f3c393aa384e57d0f043713c0843c4d118471
jjochim <janusz.jochim@gmail.com>
2016-10-28 15:02:39 +0200

    mailer production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'cinema-managment-system.herokuapp.com', port: 33544 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => "cinema.managment.system@gmail.com",
+      :password             => "Managment",
+      :authentication       => "plain",
+      :enable_starttls_auto => true
+  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
