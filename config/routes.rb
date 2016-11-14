03ba2d214b78922b1e6f43800aa6e43396fde329
jjochim <janusz.jochim@gmail.com>
2016-11-15 00:01:38 +0100

    dodanie ekranu

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
   devise_for :users
   resources :movies
   resources :infos
@@ -12,6 +16,11 @@ Rails.application.routes.draw do
   resources :seatings
   resources :rooms
   resources :categories
+  resources :users
   root to: 'movies#index'
+  # get '/users/' => 'users#index', as: :users
+  # patch '/users/:id/edit' => 'users#edit', as: :users_edit
+  # post  '/users/new' => 'users#new', as: :users_new
+  # get  '/users/:id', to:   'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
